0036eb0ebc44cc8e2dfa50984cac77899ddc6e13
jchristopherson <jchristopherson@hotmail.com>
2023-05-23 10:20:59 -0500

    Add tests

--- test/friction_test.f90
+++ test/friction_test.f90
@@ -1,3 +1,19 @@
 program test
+    use friction_model_tests
     implicit none
+
+    ! Local Variables
+    logical :: check
+    integer(int32) :: flag
+
+    ! Initialization
+    flag = 0
+
+    ! Process
+    check = test_coulomb()
+    if (.not.check) flag = 1
+
+
+    ! End
+    stop flag
 end program
