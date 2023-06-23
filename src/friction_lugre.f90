1adc14c58e951267eb4fd139b711bbc5cc74a716
jchristopherson <jchristopherson@hotmail.com>
2023-06-23 13:34:10 -0500

    Update

--- src/friction_lugre.f90
+++ src/friction_lugre.f90
@@ -9,11 +9,12 @@ module function lg_eval(this, t, x, dxdt, nrm, svars) result(rst)
     real(real64) :: rst
 
     ! Local Variables
-    real(real64) :: dsdt(1)
+    real(real64) :: s1, dsdt(1)
 
     ! Process
     call this%state(t, x, dxdt, nrm, svars, dsdt)
-    rst = nrm * (this%stiffness * svars(1) + this%damping * dsdt(1) + &
+    s1 = this%damping * exp(-(dxdt / this%stribeck_velocity)**2)
+    rst = nrm * (this%stiffness * svars(1) + s1 * dsdt(1) + &
         this%viscous_damping * dxdt)
 end function
 
