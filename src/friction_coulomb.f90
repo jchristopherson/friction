cfc5fd31970972d88d87b1dd0545fb9d33cf0fa8
Jason Christopherson <jchristopherson@hotmail.com>
2024-09-18 15:33:03 -0500

    Restructure

--- src/friction_coulomb.f90
+++ src/friction_coulomb.f90
@@ -1,12 +1,61 @@
-submodule (friction) friction_coulomb
+module friction_coulomb
+    use iso_fortran_env
+    use friction_core
+    use ferror
     implicit none
+    private
+    public :: coulomb_model
+
+    type, extends(friction_model) :: coulomb_model
+        !! Defines the basic Coulomb friction model.
+        !!
+        !! The Coulomb model is defined as follows.
+        !!
+        !! $$ F = sgn{ \left( v \right)} \mu_{c} N $$
+        !!
+        !! where:
+        !!    
+        !! \( F = \) Friction Force 
+        !!    
+        !! \( N = \) Normal Force
+        !! 
+        !! \( v = \) Velocity
+        !!
+        !! \( \mu_c = \) Coulomb Friction Coefficient
+        real(real64) :: friction_coefficient
+            !! The Coulomb friction coefficient.
+    contains
+        procedure, public :: evaluate => cf_eval
+        procedure, public :: has_internal_state => cf_has_state_vars
+        procedure, public :: state => cf_state_model
+        procedure, public :: to_array => cf_to_array
+        procedure, public :: from_array => cf_from_array
+        procedure, public :: parameter_count => cf_parameter_count
+        procedure, public :: get_state_variable_count => cf_get_state_var_count
+    end type
+
 contains
 ! ------------------------------------------------------------------------------
-module function cf_eval(this, t, x, dxdt, nrm, svars) result(rst)
+function cf_eval(this, t, x, dxdt, nrm, svars) result(rst)
+    !! Evaluates the friction model given the defined parameter state.
     class(coulomb_model), intent(inout) :: this
-    real(real64), intent(in) :: t, x, dxdt, nrm
+        !! The coulomb_model object.
+    real(real64), intent(in) :: t
+        !! The current simulation time value.
+    real(real64), intent(in) :: x
+        !! The current value of the relative position between
+        !! the contacting bodies.
+    real(real64), intent(in) :: dxdt
+        !! The current value of the relative velocity between
+        !! the contacting bodies.
+    real(real64), intent(in) :: nrm
+        !! The current normal force between the contacting 
+        !! bodies.
     real(real64), intent(in), optional, dimension(:) :: svars
+        !! An optional array containing any internal state
+        !! variables the model may rely upon.
     real(real64) :: rst
+        !! The friction force.
 
     if (dxdt /= 0.0d0) then
         rst = this%friction_coefficient * nrm * sign(1.0d0, dxdt)
@@ -16,50 +65,95 @@ module function cf_eval(this, t, x, dxdt, nrm, svars) result(rst)
 end function
 
 ! ------------------------------------------------------------------------------
-pure module function cf_has_state_vars(this) result(rst)
+pure function cf_has_state_vars(this) result(rst)
+    !! Returns a value stating if the model relies upon internal
+    !! state variables.
     class(coulomb_model), intent(in) :: this
+        !! The coulomb_model object.
     logical :: rst
+        !! Returns true if the model utilizes internal state variables;
+        !! else, returns false.
     rst = .false.
 end function
 
 ! ------------------------------------------------------------------------------
-module subroutine cf_state_model(this, t, x, dxdt, nrm, svars, dsdt)
+subroutine cf_state_model(this, t, x, dxdt, nrm, svars, dsdt)
+    !! Evaluates the time derivatives of the internal friction state model.
     class(coulomb_model), intent(inout) :: this
-    real(real64), intent(in) :: t, x, dxdt, nrm
+        !! The coulomb_model object.
+    real(real64), intent(in) :: t
+        !! The current simulation time value.
+    real(real64), intent(in) :: x
+        !! The current value of the relative position between
+        !! the contacting bodies.
+    real(real64), intent(in) :: dxdt
+        !! The current value of the relative velocity between
+        !! the contacting bodies.
+    real(real64), intent(in) :: nrm
+        !! The current normal force between the contacting 
+        !! bodies.
     real(real64), intent(in), dimension(:) :: svars
+        !! An N-element array containing any internal state
+        !! variables the model may rely upon.
     real(real64), intent(out), dimension(:) :: dsdt
+        !! An N-element array where the state variable 
+        !! derivatives are to be written.
     dsdt = 0.0d0
 end subroutine
 
 ! ------------------------------------------------------------------------------
-module subroutine cf_to_array(this, x, err)
+subroutine cf_to_array(this, x, err)
+    !! Converts the parameters of the friction model into an array.
     class(coulomb_model), intent(in) :: this
+        !! The coulomb_model object.
     real(real64), intent(out), dimension(:) :: x
+        !! The array used to store the parameters.  See @ref
+        !! parameter_count to determine the size of this array.
     class(errors), intent(inout), optional, target :: err
+        !! An optional errors-based object that if provided 
+        !! can be used to retrieve information relating to any errors 
+        !! encountered during execution. If not provided, a default 
+        !! implementation of the errors class is used internally to
+        !! provide error handling.
     x(1) = this%friction_coefficient
 end subroutine
 
 ! ------------------------------------------------------------------------------
-module subroutine cf_from_array(this, x, err)
+subroutine cf_from_array(this, x, err)
+    !! Converts the parameters of the friction model into an array.
     class(coulomb_model), intent(inout) :: this
+        !! The coulomb_model object.
     real(real64), intent(in), dimension(:) :: x
+        !! The array used to store the parameters.  See
+        !! parameter_count to determine the size of this array.
     class(errors), intent(inout), optional, target :: err
+        !! An optional errors-based object that if provided 
+        !! can be used to retrieve information relating to any errors 
+        !! encountered during execution. If not provided, a default 
+        !! implementation of the errors class is used internally to
+        !! provide error handling.
     this%friction_coefficient = x(1)
 end subroutine
 
 ! ------------------------------------------------------------------------------
-pure module function cf_parameter_count(this) result(rst)
+pure function cf_parameter_count(this) result(rst)
+    !! Gets the number of model parameters.
     class(coulomb_model), intent(in) :: this
+        !! The coulomb_model object.
     integer(int32) :: rst
+        !! The number of model parameters.
     rst = 1
 end function
 
 ! ------------------------------------------------------------------------------
-pure module function cf_get_state_var_count(this) result(rst)
+pure function cf_get_state_var_count(this) result(rst)
+    !! Gets the number of internal state variables used by the model.
     class(coulomb_model), intent(in) :: this
+        !! The coulomb_model object.
     integer(int32) :: rst
+        !! The internal state variable count.
     rst = 0
 end function
 
 ! ------------------------------------------------------------------------------
-end submodule
+end module
--- src/friction_modified_stribeck.f90
+++ src/friction_modified_stribeck.f90
@@ -1,12 +1,80 @@
-submodule (friction) friction_modified_stribeck
+module friction_modified_stribeck
+    use iso_fortran_env
+    use friction_core
+    use friction_stribeck
+    use friction_errors
+    use ferror
     implicit none
+    private
+    public :: modified_stribeck_model
+
+    type, extends(stribeck_model) :: modified_stribeck_model
+        !! Defines a modification of the Stribeck model to account for 
+        !! presliding displacement.  The presliding region utilizes a Maxwell
+        !! type model then transitions to a traditional Stribeck model as
+        !! slipping occurs.
+        !!
+        !! The model is defined as follows.
+        !!
+        !! $$ F = k \delta $$
+        !! $$ delta_{i+1} = \text{sgn} \left( x_{i+1} - x_{i} + \delta_{i} \right) \min \left( x_{i+1} - x_{i} + \delta_{i}, g(v) \right) $$
+        !! $$ g(v) = a_1 + a_2 e^{-|v / v_s|^2} $$
+        !! $$ a_{1} = \frac{\mu_c N}{k} $$
+        !! $$ a_{2} = \frac{\mu_s N - \mu_c N}{k} $$
+        !!
+        !! where:
+        !!    
+        !! \( F = \) Friction Force 
+        !!    
+        !! \( N = \) Normal Force
+        !! 
+        !! \( v = \) Velocity
+        !!
+        !! \( \mu_c = \) Coulomb Friction Coefficient
+        !!
+        !! \( \mu_s = \) Static Friction Coefficient
+        !!
+        !! \( k = \) Stiffness
+        !!
+        !! \( v_s = \) Stribeck Velocity Coefficient
+
+        real(real64) :: stiffness
+            !! The stiffness term.
+
+        ! Private, internal variables
+        real(real64), private :: x_prev = 0.0d0
+        real(real64), private :: d_prev = 0.0d0
+    contains
+        procedure, public :: evaluate => msf_eval
+        procedure, public :: to_array => msf_to_array
+        procedure, public :: from_array => msf_from_array
+        procedure, public :: parameter_count => msf_parameter_count
+        procedure, public :: reset => msf_reset
+    end type
+
 contains
 ! ------------------------------------------------------------------------------
-module function msf_eval(this, t, x, dxdt, nrm, svars) result(rst)
+function msf_eval(this, t, x, dxdt, nrm, svars) result(rst)
+    !! Evaluates the friction model given the defined parameter
+            !! state.
     class(modified_stribeck_model), intent(inout) :: this
-    real(real64), intent(in) :: t, x, dxdt, nrm
+        !! The modified_stribeck_model object.
+    real(real64), intent(in) :: t
+        !! The current simulation time value.
+    real(real64), intent(in) :: x
+        !! The current value of the relative position between
+        !! the contacting bodies.
+    real(real64), intent(in) :: dxdt
+        !! The current value of the relative velocity between
+        !! the contacting bodies.
+    real(real64), intent(in) :: nrm
+        !! The current normal force between the contacting 
+        !! bodies.
     real(real64), intent(in), optional, dimension(:) :: svars
+        !! An optional array containing any internal state
+        !! variables the model may rely upon.
     real(real64) :: rst
+        !! The friction force.
 
     real(real64) :: gv, a1, a2, zeta, delta
 
@@ -23,10 +91,30 @@ module function msf_eval(this, t, x, dxdt, nrm, svars) result(rst)
 end function
 
 ! ------------------------------------------------------------------------------
-module subroutine msf_to_array(this, x, err)
+subroutine msf_to_array(this, x, err)
+    !! Converts the parameters of the friction model into an array.
     class(modified_stribeck_model), intent(in) :: this
+        !! The modified_stribeck_model object.
     real(real64), intent(out), dimension(:) :: x
+        !! The array used to store the parameters.  See parameter_count 
+        !! to determine the size of this array.  The parameter order is
+        !! as follows.
+        !!
+        !! 1. static_friction_coefficient
+        !!
+        !! 2. coulomb_friction_coefficient
+        !!
+        !! 3. stribeck_velocity
+        !!
+        !! 4. viscous_damping
+        !!
+        !! 5. stiffness
     class(errors), intent(inout), optional, target :: err
+        !! An optional errors-based object that if provided 
+        !! can be used to retrieve information relating to any errors 
+        !! encountered during execution. If not provided, a default 
+        !! implementation of the errors class is used internally to
+        !! provide error handling.
     x(1) = this%static_friction_coefficient
     x(2) = this%coulomb_friction_coefficient
     x(3) = this%stribeck_velocity
@@ -35,10 +123,30 @@ module subroutine msf_to_array(this, x, err)
 end subroutine
 
 ! ------------------------------------------------------------------------------
-module subroutine msf_from_array(this, x, err)
+subroutine msf_from_array(this, x, err)
+    !! Converts an array into the parameters for the friction model.
     class(modified_stribeck_model), intent(inout) :: this
+        !! The modified_stribeck_model object.
     real(real64), intent(in), dimension(:) :: x
+        !! The array of parameters.  See parameter_count 
+        !! to determine the size of this array.  The parameter order is
+        !! as follows.
+        !!
+        !! 1. static_friction_coefficient
+        !!
+        !! 2. coulomb_friction_coefficient
+        !!
+        !! 3. stribeck_velocity
+        !!
+        !! 4. viscous_damping
+        !!
+        !! 5. stiffness
     class(errors), intent(inout), optional, target :: err
+        !! An optional errors-based object that if provided 
+        !! can be used to retrieve information relating to any errors 
+        !! encountered during execution. If not provided, a default 
+        !! implementation of the errors class is used internally to
+        !! provide error handling.
     this%static_friction_coefficient = x(1)
     this%coulomb_friction_coefficient = x(2)
     this%stribeck_velocity = x(3)
@@ -47,18 +155,23 @@ module subroutine msf_from_array(this, x, err)
 end subroutine
 
 ! ------------------------------------------------------------------------------
-pure module function msf_parameter_count(this) result(rst)
+pure function msf_parameter_count(this) result(rst)
+    !! Gets the number of model parameters.
     class(modified_stribeck_model), intent(in) :: this
+        !! The modified_stribeck_model object.
     integer(int32) :: rst
+        !! The number of model parameters.
     rst = 5
 end function
 
 ! ------------------------------------------------------------------------------
-module subroutine msf_reset(this)
+subroutine msf_reset(this)
+    !! Resets the friction model to it's original state.
     class(modified_stribeck_model), intent(inout) :: this
+        !! The modified_stribeck_model object.
     this%x_prev = 0.0d0
     this%d_prev = 0.0d0
 end subroutine
 
 ! ------------------------------------------------------------------------------
-end submodule
+end module
