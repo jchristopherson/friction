0ea6b4313e5158768340f08c5027abb43b3cfc2d
jchristopherson <jchristopherson@hotmail.com>
2026-08-20 19:23:11 +0000

    Deploying to gh-pages from  @ c6ca1e48b2a00cc07654c64ae4c1cb9e907651b4 🚀

--- src/friction_lugre.f90
+++ src/friction_lugre.f90
@@ -97,12 +97,11 @@ function lg_eval(this, t, x, dxdt, nrm, svars) result(rst)
         !! The friction force.
 
     ! Local Variables
-    ! real(real64) :: s1, dsdt(1)
     real(real64) :: dsdt(1)
 
     ! Process
+    if (.not.present(svars)) error stop FRICTION_INVALID_OPERATION_ERROR
     call this%state(t, x, dxdt, nrm, svars, dsdt)
-    ! s1 = this%damping * exp(-(dxdt / this%stribeck_velocity)**2)
     rst = this%stiffness * svars(1) + this%damping * dsdt(1) + &
         this%viscous_damping * dxdt
 end function
